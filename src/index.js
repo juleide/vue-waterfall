9191e012ed21ed7c9445f58fd552edec53832151
julei <741337937@qq.com>
2019-02-25 16:37:24 +0800

    format 4 space

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import Waterfall from './waterfall'
 import WaterfallSlot from './waterfall-slot'
 
 module.exports = {
-  Waterfall: Waterfall,
-  WaterfallSlot: WaterfallSlot,
-  waterfall: Waterfall,
-  waterfallSlot: WaterfallSlot
+    Waterfall: Waterfall,
+    WaterfallSlot: WaterfallSlot,
+    waterfall: Waterfall,
+    waterfallSlot: WaterfallSlot
 }
